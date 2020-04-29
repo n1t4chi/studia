796a1f932886a59f2fcd4217febd544446240532
n1t4chi <O^A%dfFafQ&J#@4&bU^@*H>
2020-04-29 10:49:57 +0200

    Semester 10/Cryptography 2020 *List 2 **Task 1 Fix up parameters

--- semestr10/krypto/kod/src/main/java/lab2/zad1/EncryptionMode.java
+++ semestr10/krypto/kod/src/main/java/lab2/zad1/EncryptionMode.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 
 public enum EncryptionMode {
     aes_cbc( "AES_128/CBC/NOPADDING", "CBC"),
-    aes_ofb( "AES_128/OFB/NOPADDING", "CTR" ),
-    aes_ctr( "AES_128/GCM/NOPADDING", "CBC" ),
+    aes_ofb( "AES_128/OFB/NOPADDING", "OFB" ),
+    aes_ctr( "AES_128/GCM/NOPADDING", "CTR" ),
     ;
     
     public static Optional<EncryptionMode> get( String name )
@@ -17,8 +17,8 @@ public enum EncryptionMode {
         ;
     }
     
-    private String algorithmName;
-    private String parameterName;
+    private final String algorithmName;
+    private final String parameterName;
     
     EncryptionMode( String algorithmName, String parameterName ) {
         
@@ -31,6 +31,10 @@ public enum EncryptionMode {
     }
     
     String getParameterName() {
+        return parameterName;
+    }
+    
+    String toHelpString() {
         return parameterName;
     }
 }
