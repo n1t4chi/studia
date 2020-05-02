1500aa0424354e81f5f8c831f73ae35f21773fae
n1t4chi <O^A%dfFafQ&J#@4&bU^@*H>
2020-05-02 03:37:14 +0200

    Semester 10/Optimization methods 2020 *List 2 **Task 3: *** prints fixes

--- semestr10/metody opt/kod/src/main/julia/lista2/zad3/zad3.jl
+++ semestr10/metody opt/kod/src/main/julia/lista2/zad3/zad3.jl
@@ -20,88 +20,76 @@ function solve(
     taskDurationPerProcessor::Matrix{Float64};
     verbose = true
 )
-    (processorCount,taskCount) = size(taskDurationPerProcessor)
-    processors = 1:processorCount
-    tasks = 1:taskCount
+    (processorCount,taskCount) = size(taskDurationPerProcessor) #done
+    processors = 1:processorCount #done
+    tasks = 1:taskCount #done
 
     model = Model(GLPK.Optimizer)
 
     #= scheduledTasks are indexes for tasks after applying schedule =#
-    scheduledTasks = 1:taskCount
+    scheduledTasks = 1:taskCount #done
     #main variables
-    @variable(model, 1 <= schedule[scheduledTasks] <= taskCount, Int)
-    @variable(model, 0 <= maxTime )
+    @variable(model, 1 <= schedule[scheduledTasks] <= taskCount, Int) #done
+    @variable(model, 0 <= maxTime ) #done
     #minimise r
-    @objective(model,Min, maxTime )
+    @objective(model,Min, maxTime ) #done
 
 
     #mapping variables between tasks and scheduledTasks
     #swapping the tasks to scheduled tasks
-    @variable(model, swaps[tasks,scheduledTasks], Bin )
+    @variable(model, swaps[tasks,scheduledTasks], Bin ) #done
     for task=tasks
-        @constraint(model, sum(swaps[task,targetTask] for targetTask in scheduledTasks ) == 1  )
+        @constraint(model, sum(swaps[task,targetTask] for targetTask in scheduledTasks ) == 1  ) #done
     end
     for targetTask=scheduledTasks
-        @constraint(model, sum(swaps[task,targetTask] for task in tasks ) == 1  )
+        @constraint(model, sum(swaps[task,targetTask] for task in tasks ) == 1  ) #done
     end
+
     #swapping the tasks values to scheduled tasks
-    @variable(model, swapsValues[tasks,scheduledTasks], Int )
+    @variable(model, swapsValues[tasks,scheduledTasks], Int ) #done
     for task=tasks
         for targetTask=scheduledTasks
-            @constraint(model, swapsValues[task,targetTask] == task * swaps[task,targetTask] )
+            @constraint(model, swapsValues[task,targetTask] == task * swaps[task,targetTask] ) #done
         end
     end
     for targetTask=scheduledTasks
-        @constraint(model, schedule[targetTask] == sum(swapsValues[task,targetTask] for task in tasks ) )
+        @constraint(model, schedule[targetTask] == sum(swapsValues[task,targetTask] for task in tasks ) ) #done
     end
     #swapping the task durations to scheduled tasks per processor
-    @variable(model, swapsDurations[processors,tasks,scheduledTasks], Int )
+    @variable(model, swapsDurations[processors,tasks,scheduledTasks], Int ) #done
     for processor=processors
         for task=tasks
             for targetTask=scheduledTasks
-                @constraint(model, swapsDurations[processor,task,targetTask] == taskDurationPerProcessor[processor,task] * swaps[task,targetTask] )
+                @constraint(model, swapsDurations[processor,task,targetTask] == taskDurationPerProcessor[processor,task] * swaps[task,targetTask] ) #done
             end
         end
     end
 
 
     #helper variables and constrinats
-    @variable(model, taskLength[processors,scheduledTasks] >= 0 )
-    @variable(model, taskStart[processors,scheduledTasks] >= 0 )
-    @variable(model, taskEnd[processors,scheduledTasks] >= 0 )
-    @variable(model, possibleTaskStart[processors,scheduledTasks,1:2] >= 0 )
+    @variable(model, taskLength[processors,scheduledTasks] >= 0 ) #done
+    @variable(model, taskStart[processors,scheduledTasks] >= 0 ) #done
+    @variable(model, taskEnd[processors,scheduledTasks] >= 0 ) #done
 
     for processor=processors
         for task=scheduledTasks
             if processor == 1
                 if task == 1
-                    @constraint(model, possibleTaskStart[processor,task,1] == 0  )
-                    @constraint(model, possibleTaskStart[processor,task,2] == 0  )
-                else
-                    @constraint(model, possibleTaskStart[processor,task,1] == taskEnd[processor,task-1] )
-                    @constraint(model, possibleTaskStart[processor,task,2] == taskEnd[processor,task-1] )
+                    @constraint(model, taskStart[processor,task] == 0  ) #done
                 end
             else
-                if task == 1
-                    @constraint(model, possibleTaskStart[processor,task,1] == taskEnd[processor-1,task] )
-                    @constraint(model, possibleTaskStart[processor,task,2] == taskEnd[processor-1,task] )
-                else
-                    @constraint(model, possibleTaskStart[processor,task,1] == taskEnd[processor-1,task] )
-                    @constraint(model, possibleTaskStart[processor,task,2] == taskEnd[processor,task-1] )
-                end
+                @constraint(model, taskStart[processor,task] >= taskEnd[processor-1,task] ) #done
             end
-            @constraint(model, taskEnd[processor,task] == taskStart[processor,task] + taskLength[processor,task] )
+            if task > 1
+                @constraint(model, taskStart[processor,task] >= taskEnd[processor,task-1] ) #done
+            end
 
-            @constraint(model, taskLength[processor,task] == sum( swapsDurations[processor,initialTask,task] for initialTask in tasks ) )
+            @constraint(model, taskEnd[processor,task] == taskStart[processor,task] + taskLength[processor,task] ) #done
+
+            @constraint(model, taskLength[processor,task] == sum( swapsDurations[processor,initialTask,task] for initialTask in tasks ) ) #done
         end
     end
-    for processor=processors
-        for task=scheduledTasks
-            @constraint(model, taskStart[processor,task] >= possibleTaskStart[processor,task,1] )
-            @constraint(model, taskStart[processor,task] >= possibleTaskStart[processor,task,2] )
-        end
-    end
-    @constraint(model, maxTime == taskEnd[processorCount,taskCount] )
+    @constraint(model, maxTime == taskEnd[processorCount,taskCount] ) #done
 
 
     if verbose
@@ -123,9 +111,9 @@ function solve(
         _swaps=value.(swaps)
         _swapsValues=value.(swapsValues)
         _swapsDurations=value.(swapsDurations)
-        _possibleTaskStart=value.(possibleTaskStart)
 
 
+        #=
         for task=tasks
             for targetTask=scheduledTasks
                 println( "_swaps[",task,",",targetTask,"]=",_swaps[task,targetTask]," (val:",_swapsValues[task,targetTask],"), delays per processor:" );
@@ -145,19 +133,37 @@ function solve(
                 end
             end
         end
-
+        =#
+        graph=""
         for processor=processors
-            println( "processor ", processor );
+            #println( "processor ", processor );
+
+            graph=string(graph,"p",lpad(processor,2,"0"),"[");
+            endLine=0
             for task=scheduledTasks
-                println( "\ttask ", _schedule[task],
+                #=
+                println( "\ttask ", Int64(_schedule[task]),
                     ", start: ",rd(_taskStart[processor,task]),
                         "(possible values: [1]=",rd(_possibleTaskStart[processor,task,1])," [2]=",rd(_possibleTaskStart[processor,task,2]),")",
                     ", end: ",rd(_taskEnd[processor,task]),
                     ", length:",rd(_taskLength[processor,task])
-                 );
+                );
+                =#
+                newStart = Int64(round(_taskStart[processor,task]));
+                if( endLine < newStart )
+                    graph=string(graph,lpad("",newStart-endLine," "))
+                    endLine = newStart
+                end
+                length = Int64(round(_taskLength[processor,task]));
+                fill = string(Int64(_schedule[task]));
+                graph=string(graph,lpad("",length,fill))
+                endLine += length
             end
+            graph=string(graph,lpad("",Int64(round(_maxTime))-endLine," "))
+            graph=string(graph,"]\n");
         end
-        println( "maxTime = ",_maxTime );
+        #println( "maxTime = ",_maxTime );
+        println( "graph\n",graph );
 
         return status, Result(
             #params
@@ -198,15 +204,38 @@ function solveAndLog( taskDurationPerProcessor::Matrix{Float64} )
     println("============end============\n\n");
 end
 
+#=
+Solution: 2 1 3
+Max time: 8.0
+p01[213333  ]
+p02[ 2221113]
+=#
+solveAndLog(
+    [
+        1. 1. 4.;
+        3. 3. 1.;
+    ]
+);
+#=
+Solution: 1 3 2
+Max time: 9.0
+p01[132222   ]
+p02[  1113222]
+=#
+solveAndLog(
+    [
+        1. 4. 1.;
+        3. 3. 1.;
+    ]
+);
 
 #=
-A -> B -> D -> C ?
-ABBDDDCCCC
- AABB DDD C
-   AAABB DDDC
-t=13
+Solution: 1 4 2 3
+Max time: 13.0
+p01[1444 223333  ]
+p02[  11444 22 3 ]
+p03[    111444223]
 =#
-println("Expected: 1->2->3->4 t=13");
 solveAndLog(
     [  #A  B  C  D
         1. 2. 4. 3.;
